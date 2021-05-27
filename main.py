ac5a509532b52533d9dbfd3b24cf4338ff8ae154
Larissa Midori <>
2021-05-27 18:47:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Testando a função enumerate
+
+frutas = ['uva', 'laranja', 'maçã', 'kiwi', 'melão', 'caqui', 'morango', 'limão']
+for i, cestaDeFrutas in enumerate(frutas):
+  print(i, cestaDeFrutas)
+
+print('-------------')
+
+carros = ['Etios', 'March', 'Celta', 'Corsa', 'Punto', 'Ka']
+for i, nomesCarros in enumerate(carros):
+  print(i, nomesCarros)
+
